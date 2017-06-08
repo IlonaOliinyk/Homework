08328da14b3f53320d9601a5abed402a0fc85d18
Ilona <ilona.oliinyk@gmail.com>
2017-06-08 17:11:24 +0300

    Update Main.java

--- src/com/ilona/homework07/Main.java
+++ src/com/ilona/homework07/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         }
 
-        System.out.println(String.format("My name is %s, haha", "IO" + random1.nextInt()));
+        System.out.println(String.format("My name is %s", "IO" + random1.nextInt()));
 
 
     }
